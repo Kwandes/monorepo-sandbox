17b18056468b0ff7052a7b95938b3510d4237895
Jan <jan.bogoryja@gmail.com>
2020-12-31 16:57:22 +0100

    Adjust code styling
    The only good bracket style for java

--- app-sandbox/apps/java-api/src/main/java/dev/hotdeals/api/ApiApplication.java
+++ app-sandbox/apps/java-api/src/main/java/dev/hotdeals/api/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
+public class ApiApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 }

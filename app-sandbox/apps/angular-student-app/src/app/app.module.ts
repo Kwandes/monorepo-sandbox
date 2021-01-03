543fa965d1532b3843a6fd26cb258c94d21ca2c1
Jan <jan.bogoryja@gmail.com>
2021-01-03 22:51:43 +0100

    Add a basic home component
    `nx generate @schematics/angular:component --name=home --project=angular-student-app`

--- app-sandbox/apps/angular-student-app/src/app/app.module.ts
+++ app-sandbox/apps/angular-student-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),

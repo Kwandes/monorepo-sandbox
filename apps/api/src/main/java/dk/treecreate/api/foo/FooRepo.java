e778c8f349948b129dc34dc0d990851ec8b4480d
Kwandes <bz.jasiek@gmail.com>
2021-03-21 22:20:40 +0100

    Revert "feat: 🎸 Add authentication stuff and foo controller&model"
    This reverts commit 5b17689ca046a8542a13c5578ed667f934e8fab8.

--- apps/api/src/main/java/dk/treecreate/api/foo/FooRepo.java
+++
@@ -1,7 +0,0 @@
-package dk.treecreate.api.foo;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface FooRepo extends PagingAndSortingRepository<Foo, Long>
-{
-}

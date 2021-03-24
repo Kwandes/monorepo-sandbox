9f6e5e9efa8583d787679d6d4f2ca231161385a8
Kwandes <bz.jasiek@gmail.com>
2021-03-24 22:14:13 +0100

    Disable okta for now, Switch from mysql db to H2

--- apps/api/src/main/java/dk/treecreate/api/ApiApplication.java
+++ apps/api/src/main/java/dk/treecreate/api/ApiApplication.java
@@ -1,7 +1,13 @@
 package dk.treecreate.api;
 
+import dk.treecreate.api.car.Car;
+import dk.treecreate.api.car.CarRepo;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +16,19 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+
+	// temp database bypass in form of a H2 in-memory database.
+  // This data would get printed to console when you run the app
+  @Bean
+  ApplicationRunner init(CarRepo repository) {
+    return args -> {
+      Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+        "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+        Car car = new Car();
+        car.setName(name);
+        repository.save(car);
+      });
+      repository.findAll().forEach(System.out::println);
+    };
+  }
 }

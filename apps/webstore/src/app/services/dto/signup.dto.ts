1a394d578904c65c5fbefc0b61cc513f8ce7f50c
Kwandes <bz.jasiek@gmail.com>
2021-03-25 13:13:09 +0100

    Add AuthService

---
+++ apps/webstore/src/app/services/dto/signup.dto.ts
@@ -0,0 +1,13 @@
+export class SignupDto {
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+  }
+  
+  export class SignupResponse {
+    email: string;
+    firstName: string;
+    lastName: string;
+  }
+  

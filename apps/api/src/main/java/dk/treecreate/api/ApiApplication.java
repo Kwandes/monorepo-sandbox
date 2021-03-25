f7030e23abb1f03dcb2485edfdcabc2849cc1ced
Kwandes <bz.jasiek@gmail.com>
2021-03-25 12:45:11 +0100

    Add cors to the api

--- apps/api/src/main/java/dk/treecreate/api/ApiApplication.java
+++ apps/api/src/main/java/dk/treecreate/api/ApiApplication.java
@@ -5,22 +5,31 @@ import dk.treecreate.api.car.CarRepo;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+  public static void main(String[] args)
+  {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 
 
-	// temp database bypass in form of a H2 in-memory database.
+  // temp database bypass in form of a H2 in-memory database.
   // This data would get printed to console when you run the app
   @Bean
-  ApplicationRunner init(CarRepo repository) {
+  ApplicationRunner init(CarRepo repository)
+  {
     return args -> {
       Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
         "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
@@ -30,5 +39,20 @@ public class ApiApplication {
       });
       repository.findAll().forEach(System.out::println);
     };
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter()
+  {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
